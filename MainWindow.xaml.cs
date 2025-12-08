27c5bde703cf7b11be2d6c019e6e040572409396
Oleh Tiuriakov <152391484+reuerendo@users.noreply.github.com>
2025-12-08 15:59:54 +0100

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,28 +9,72 @@ namespace VirtualKeyboard;
 public sealed partial class MainWindow : Window
 {
     [DllImport("user32.dll")]
-    static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+    static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+    [DllImport("user32.dll")]
+    static extern IntPtr GetForegroundWindow();
 
     [DllImport("user32.dll", SetLastError = true)]
     static extern uint GetDpiForWindow(IntPtr hwnd);
 
+    [DllImport("user32.dll")]
+    static extern bool SetForegroundWindow(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+    [DllImport("kernel32.dll")]
+    static extern uint GetCurrentThreadId();
+
+    [DllImport("user32.dll")]
+    static extern bool AttachThreadInput(uint idAttach, uint idAttachTo, bool fAttach);
+
+    const int INPUT_KEYBOARD = 1;
     const uint KEYEVENTF_KEYUP = 0x0002;
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct INPUT
+    {
+        public int type;
+        public InputUnion u;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    struct InputUnion
+    {
+        [FieldOffset(0)]
+        public KEYBDINPUT ki;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct KEYBDINPUT
+    {
+        public ushort wVk;
+        public ushort wScan;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+
+    private IntPtr _targetWindow = IntPtr.Zero;
+    private IntPtr _thisWindowHandle;
 
     public MainWindow()
     {
         this.InitializeComponent();
         Title = "Virtual Keyboard";
         
+        // Get window handle
+        _thisWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(this);
+        
         // Get the AppWindow for advanced windowing features
         var appWindow = this.AppWindow;
         
-        // Get window handle and DPI for proper scaling
-        IntPtr hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
-        uint dpi = GetDpiForWindow(hwnd);
+        // Get DPI for proper scaling
+        uint dpi = GetDpiForWindow(_thisWindowHandle);
         float scalingFactor = dpi / 96f;
         
         // Set window size in physical pixels (accounting for DPI scaling)
-        // Logical size: 760x330, scaled to physical pixels
         int physicalWidth = (int)(760 * scalingFactor);
         int physicalHeight = (int)(330 * scalingFactor);
         
@@ -48,25 +92,115 @@ public sealed partial class MainWindow : Window
             // Disable maximize button
             presenter.IsMaximizable = false;
         }
+
+        // Store target window on activation
+        this.Activated += MainWindow_Activated;
+    }
+
+    private void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
+    {
+        // When our window gets activated, we want to remember the previous foreground window
+        if (args.WindowActivationState != WindowActivationState.Deactivated)
+        {
+            // Store the current foreground window as target (before we were activated)
+            IntPtr foreground = GetForegroundWindow();
+            if (foreground != _thisWindowHandle && foreground != IntPtr.Zero)
+            {
+                _targetWindow = foreground;
+            }
+        }
     }
 
     private void KeyButton_Click(object sender, RoutedEventArgs e)
     {
         if (sender is Button button && button.Tag is string keyCode)
         {
-            SendKey(keyCode);
+            SendKeyToTarget(keyCode);
         }
     }
 
-    private void SendKey(string key)
+    private void SendKeyToTarget(string key)
     {
+        // If we don't have a target window yet, try to get it
+        if (_targetWindow == IntPtr.Zero)
+        {
+            _targetWindow = GetForegroundWindow();
+            if (_targetWindow == _thisWindowHandle)
+            {
+                return; // Can't send to ourselves
+            }
+        }
+
+        // Temporarily switch focus to target window
+        IntPtr previousWindow = GetForegroundWindow();
+        BringWindowToForeground(_targetWindow);
+
+        // Send the key
         byte vk = GetVirtualKeyCode(key);
         if (vk != 0)
         {
-            // Press key
-            keybd_event(vk, 0, 0, 0);
-            // Release key
-            keybd_event(vk, 0, KEYEVENTF_KEYUP, 0);
+            INPUT[] inputs = new INPUT[2];
+            
+            // Key down
+            inputs[0].type = INPUT_KEYBOARD;
+            inputs[0].u.ki.wVk = vk;
+            inputs[0].u.ki.wScan = 0;
+            inputs[0].u.ki.dwFlags = 0;
+            inputs[0].u.ki.time = 0;
+            inputs[0].u.ki.dwExtraInfo = IntPtr.Zero;
+            
+            // Key up
+            inputs[1].type = INPUT_KEYBOARD;
+            inputs[1].u.ki.wVk = vk;
+            inputs[1].u.ki.wScan = 0;
+            inputs[1].u.ki.dwFlags = KEYEVENTF_KEYUP;
+            inputs[1].u.ki.time = 0;
+            inputs[1].u.ki.dwExtraInfo = IntPtr.Zero;
+            
+            SendInput(2, inputs, Marshal.SizeOf(typeof(INPUT)));
+        }
+
+        // Return focus to keyboard window
+        System.Threading.Tasks.Task.Delay(50).ContinueWith(_ => 
+        {
+            DispatcherQueue.TryEnqueue(() => 
+            {
+                BringWindowToForeground(_thisWindowHandle);
+            });
+        });
+    }
+
+    private void BringWindowToForeground(IntPtr hWnd)
+    {
+        if (hWnd == IntPtr.Zero) return;
+
+        IntPtr foregroundWindow = GetForegroundWindow();
+        if (foregroundWindow == hWnd) return;
+
+        uint foregroundThreadId = GetWindowThreadProcessId(foregroundWindow, out _);
+        uint targetThreadId = GetWindowThreadProcessId(hWnd, out _);
+        uint currentThreadId = GetCurrentThreadId();
+
+        // Attach input threads to allow setting foreground
+        if (foregroundThreadId != currentThreadId)
+        {
+            AttachThreadInput(currentThreadId, foregroundThreadId, true);
+        }
+        if (targetThreadId != currentThreadId && targetThreadId != foregroundThreadId)
+        {
+            AttachThreadInput(currentThreadId, targetThreadId, true);
+        }
+
+        SetForegroundWindow(hWnd);
+
+        // Detach input threads
+        if (foregroundThreadId != currentThreadId)
+        {
+            AttachThreadInput(currentThreadId, foregroundThreadId, false);
+        }
+        if (targetThreadId != currentThreadId && targetThreadId != foregroundThreadId)
+        {
+            AttachThreadInput(currentThreadId, targetThreadId, false);
         }
     }
 
